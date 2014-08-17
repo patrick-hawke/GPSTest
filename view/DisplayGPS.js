38e2f68bf99e1a2b0ff70f37e6d9bfe9b72fb060
patrick-hawke <patrick.hawke@my.jcu.edu.au>
2014-08-17 16:19:59 +1000

    view directory and index with GPS js code to display coordinates and update every 2 seconds.

---
+++ view/DisplayGPS.js
@@ -0,0 +1,38 @@
+/**
+ * Prototype to display the users current GPS coordinates
+ */
+
+(function() {
+	var x = document.getElementById("coordinates");
+
+    // Executes when the page loads
+	window.onload = function() {
+		setInterval(function () {
+        	getLocation();
+//        	showPosition();
+			
+		}, 2000);
+       
+        // Disable mouse right click by returning false. Context menu isn't displayed
+        canvas.oncontextmenu = function () {
+            return false;
+    };
+
+    // Get the the current location or return an error
+	function getLocation() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(showPosition);
+	    } else {
+	        x.innerHTML = "Geolocation is not supported by this browser.";
+	    }
+	}
+	// Present the location on the screen
+	 function showPosition(position) {
+	    x.innerHTML = "Latitude: " + position.coords.latitude + 
+	    "<br>Longitude: " + position.coords.longitude; 
+	 }
+	 
+	};
+
+
+}());
